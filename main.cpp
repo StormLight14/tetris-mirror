b63461eb4ad4c9aba9464b4da0c29897b767f9a6
StormLight14 <stormpower44@protonmail.com>
2024-11-20 08:21:41 -0700

    make input be on a separate thread

--- main.cpp
+++ main.cpp
@@ -1,17 +1,37 @@
 #include "Game.hpp"
 #include <curses.h>
+#include <thread>
+#include <mutex> // for some thread safety stuff i kinda get
 
 int main() {
   Game game;
   game.initCurses();
 
-  // main game loop
-  while (game.playing) {
+  std::mutex gameMutex;
+
+  std::thread inputThread([&]() { // uses a lambda thingy to run input code on a separate thread
+    while (true) {
+      std::lock_guard<std::mutex> lock(gameMutex); // locks mutex for thread safety
+      game.handleInput();
+
+      if (!game.playing) {
+        break;
+      }
+    }
+  });
+
+  while (true) {
+    std::lock_guard<std::mutex> lock(gameMutex);
     game.handleGravity();
-    game.handleInput();
     game.displayGame();
+
+    if (!game.playing) {
+      break;
+    }
   }
 
+  inputThread.join(); // make sure input thread will end
   endwin(); // reset terminal settings to default
+  
   return 0;
 }
